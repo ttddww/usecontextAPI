fa8cfab4706025ec3cc73d0244efd6d9ac078fbc
ttddww1977 <ttddww1977@yahoo.com>
2024-01-13 08:04:20 -0500

    Update ParentCounter.js

--- src/ParentCounter.js
+++ src/ParentCounter.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import CounterDisplay from './CounterDisplay'
+import React from "react";
+import CounterDisplay from "./CounterDisplay";
 
-function ParentCounter({countData}) {
+function ParentCounter({ countData }) {
   return (
-    <div className='App'>
-        <hr/> <br/> <br/>
-        <h1>I am the parent component</h1>
-        <CounterDisplay data={countData}/>
+    <div className="App">
+      <hr /> <br /> <br />
+      <h1>I am the parent component</h1>
+      <CounterDisplay data={countData} />
     </div>
-  )
+  );
 }
 
-export default ParentCounter
+export default ParentCounter;
